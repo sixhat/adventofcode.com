3d4fb2de445551f1e56d32e3d874c7079df6a853
Dave <david@sixhat.net>
2023-05-21 13:31:20 +0100

    day 6

---
+++ 2022/day/6/day6.jl
@@ -0,0 +1,25 @@
+#= https://adventofcode.com/2022/day/6
+Is this is a Queue of size 3 or a Set? Again Sets are best.
+=#
+using DataStructures
+
+function marker(n, line)
+	for i = n:length(line)
+    q = Set{Char}()
+		for j = 0:(n-1)
+    	push!(q, line[i-j])
+		end
+    if length(q) == n
+      println("Marker ", n , " => " ,i)
+      break
+    end
+  end
+end
+
+function day6()
+  line = readline("input.data")
+  marker(4, line)
+	marker(14, line)
+end
+
+day6()

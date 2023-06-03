3ff5c2727ce8f8be7f364790f444ba96b98aabac
Dave <david@sixhat.net>
2023-06-03 15:14:56 +0100

    fix indentation

--- 2022/day/10/day10.jl
+++ 2022/day/10/day10.jl
@@ -4,34 +4,33 @@
 lines = readlines("input.data")
 
 function run(lines)
-  cycles = 0
-  xR = 1
-  strength = 0
-  for line in lines
-    el = split(line)
-    for i = 1:length(el)
-      if cycles % 40 in [xR - 1, xR, xR + 1]
-        print("#")
-      else
-        print(" ")
-      end
+    cycles = 0
+    xR = 1
+    strength = 0
+    for line in lines
+        el = split(line)
+        for i = 1:length(el)
+            if cycles % 40 in [xR - 1, xR, xR + 1]
+                print("#")
+            else
+                print(" ")
+            end
 
-      cycles += 1
+            cycles += 1
 
-      if cycles in [20, 60, 100, 140, 180, 220]
-        strength += cycles * xR
-      end
+            if cycles in [20, 60, 100, 140, 180, 220]
+                strength += cycles * xR
+            end
 
-      if cycles in [40, 80, 120, 160, 200, 240]
-        println("")
-      end
+            if cycles in [40, 80, 120, 160, 200, 240]
+                println("")
+            end
+        end
+        if length(el) == 2
+            xR += parse(Int, el[2])
+        end
     end
-    if length(el) == 2
-      xR += parse(Int, el[2])
-    end
-    # println(cycles, "\t", xR, "\t", line)
-  end
-  strength
+    strength
 end
 
 println("Part 1: ", run(lines))

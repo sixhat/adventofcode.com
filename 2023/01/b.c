537ce9cdea8437ebeddae26a1c9d76d82b35c9d8
Dave <sixhat@users.noreply.github.com>
2023-12-01 19:34:27 +0000

    Added C version

---
+++ 2023/01/b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    const char *file_name = "input";
+    FILE *file = fopen(file_name, "r");
+    char line[256];
+
+    while (fgets(line, sizeof(line), file))
+    {
+        printf("%s", line);
+    }
+
+	fclose(file);
+    return 0;
+}

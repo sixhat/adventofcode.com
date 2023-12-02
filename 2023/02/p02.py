8462151df00b8f1beb7b9f5b858f84e8a18475c8
Dave <sixhat@users.noreply.github.com>
2023-12-02 12:47:19 +0000

    solved parts A and B

--- 2023/02/p02.py
+++ 2023/02/p02.py
@@ -3,34 +3,41 @@ import re
 # https://adventofcode.com/2023/day/2
 
 
-def processa_linha(linha, r, g, b):
+def processa_linha(linha):
     # regex -find game number return games numbers possible <=r,g,b
     gn = re.compile("Game (\d+)")
     rer = re.compile("(\d+) red")
     reg = re.compile("(\d+) green")
     reb = re.compile("(\d+) blue")
-    
+
     game = int(gn.findall(linha)[0])
     red = max([int(x) for x in rer.findall(linha)])
     green = max([int(x) for x in reg.findall(linha)])
     blue = max([int(x) for x in reb.findall(linha)])
-    
-    if red <= r and green <= g and blue <= b:
-        return game
-    else:
-        return 0
+
+    return game, red, green, blue
+
 
 def parte_a(data):
     total = 0
     for linha in data:
-        total += processa_linha(linha.strip(), 12, 13, 14)
-    print ("-- Total parte A ", total)
+        gn, red, green, blue = processa_linha(linha.strip())
+        if red <= 12 and green <= 13 and blue <= 14:
+            total += gn
+    print("-- Total parte A ", total)
+
 
 def parte_b(data):
+    total = 0
     for linha in data:
-        processa_linha(linha.strip(), 12, 13, 14)
+        gn, red, green, blue = processa_linha(linha.strip())
+        total += red * green * blue
+    print("-- Total parte B ", total)
 
 
 inpf = open(os.path.dirname(os.path.realpath(__file__)) + "/input", "r")
-parte_a(inpf)
-# parte_b(inpf)
+data = inpf.readlines()
+inpf.close()
+
+parte_a(data)
+parte_b(data)

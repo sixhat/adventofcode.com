94037d69f8893750e0b1b8b8597ee3d88cd6cbb7
Dave <sixhat@users.noreply.github.com>
2023-12-03 11:43:17 +0000

    Solved part B

--- 2023/03/p03.py
+++ 2023/03/p03.py
@@ -1,12 +1,12 @@
 import os
 import re
-from typing import List
 
 red = re.compile("(\d+)")
 ren = re.compile("[^.\d]+")
+restar = re.compile("\*{1}")
 
 
-def dim(data):
+def dim(data: list[str]) -> dict[str, int]:
     "Returns xdim, ydim"
     return {"x": len(data[0]), "y": len(data)}
 
@@ -23,30 +23,76 @@ def check_match(m: re.Match, y: int) -> int:
     for i in range(ys, ye):
         ns += data[i][xs:xe].strip()
     nsv = ren.search(ns)
-    if nsv != None:
+    if nsv is not None:
         return val
     else:
         return 0
 
 
-def parte_a(data):
-    # Find numbers and their indicies (can be done with regex)
+def find_numbers(data: list[str]) -> list[tuple[int, int, int]]:
     d = dim(data)
-    total = 0
+    numbers = []
     for y in range(d["y"]):
         matches = red.finditer(data[y])
         for m in matches:
-            # print(m)
+            numbers.append((y, m.start(), m.end()))
+    return numbers
+
+
+def find_gears(data: list[str]) -> list[tuple[int, int, int]]:
+    d = dim(data)
+    stars = []
+    for y in range(d["y"]):
+        matches = restar.finditer(data[y])
+        for m in matches:
+            stars.append((y, m.start(), m.end()))
+    return stars
+
+
+def parte_a(data: list[str]) -> None:
+    # Find numbers and their indicies (can be done with regex)
+    d = dim(data)
+    total: int = 0
+    for y in range(d["y"]):
+        matches = red.finditer(data[y])
+        for m in matches:
             total += check_match(m, y)
     print("-- Total parte A:\t", total)
 
 
-def parte_b(data):
-    pass
+def check_gear(star: tuple[int, int, int], nums: list[tuple[int, int, int]]):
+    spokes = []
+    for n in nums:
+        if abs(star[0] - n[0]) > 1:
+            continue
+        if (
+            star[1] - n[2] == 0
+            or n[1] - star[1] == 1
+            or (star[1] >= n[1] and n[2] >= star[1])
+        ):
+            spokes.append(n)
+    if len(spokes) == 2:
+        return spokes
+    else:
+        return None
+
+
+def parte_b(data: list[str]) -> None:
+    all_numbers = find_numbers(data)
+    all_gears = find_gears(data)
+    total_gear_ratios = 0
+    for star in all_gears:
+        spokes = check_gear(star, all_numbers)
+        if spokes is not None:
+            spoke0 = int(data[spokes[0][0]][spokes[0][1] : spokes[0][2]])
+            spoke1 = int(data[spokes[1][0]][spokes[1][1] : spokes[1][2]])
+            total_gear_ratios += spoke0 * spoke1
+    print("-- Total parte B\t", total_gear_ratios)
 
 
 inpf = open(os.path.dirname(os.path.realpath(__file__)) + "/input", "r")
-data: List[str] = inpf.readlines()
+# inpf = open(os.path.dirname(os.path.realpath(__file__)) + "/demo", "r")
+data: list[str] = inpf.readlines()
 inpf.close()
 
 parte_a(data)

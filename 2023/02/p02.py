4b9f135b17573c638ec3686427eff21366415799
Dave <sixhat@users.noreply.github.com>
2023-12-03 12:29:55 +0000

    Update p02.py

--- 2023/02/p02.py
+++ 2023/02/p02.py
@@ -1,7 +1,5 @@
 import os
 import re
-from typing import List, Tuple
-# https://adventofcode.com/2023/day/2
 
 gn = re.compile("Game (\d+)")
 rer = re.compile("(\d+) red")
@@ -9,7 +7,7 @@ reg = re.compile("(\d+) green")
 reb = re.compile("(\d+) blue")
 
 
-def processa_linha(linha: str) -> Tuple[int, int, int, int]:
+def processa_linha(linha: str) -> tuple[int, int, int, int]:
     game = int(gn.findall(linha)[0])
     red = max([int(x) for x in rer.findall(linha)])
     green = max([int(x) for x in reg.findall(linha)])
@@ -17,7 +15,7 @@ def processa_linha(linha: str) -> Tuple[int, int, int, int]:
     return game, red, green, blue
 
 
-def parte_a(data: List[str]) -> None:
+def parte_a(data: list[str]) -> None:
     total: int = 0
     for linha in data:
         gn, red, green, blue = processa_linha(linha.strip())
@@ -26,7 +24,7 @@ def parte_a(data: List[str]) -> None:
     print("-- Total parte A ", total)
 
 
-def parte_b(data: List[str]) -> None:
+def parte_b(data: list[str]) -> None:
     total: int = 0
     for linha in data:
         _, red, green, blue = processa_linha(linha.strip())
@@ -34,9 +32,11 @@ def parte_b(data: List[str]) -> None:
     print("-- Total parte B ", total)
 
 
-inpf = open(os.path.dirname(os.path.realpath(__file__)) + "/input", "r")
-data: List[str] = inpf.readlines()
-inpf.close()
-
+data: list[str] = (
+    open(os.path.dirname(os.path.realpath(__file__)) + "/input", "r")
+    .read()
+    .strip()
+    .split("\n")
+)
 parte_a(data)
 parte_b(data)

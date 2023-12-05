cd40bb12877b0130b805a10dcb139b3d8f378376
Dave <sixhat@users.noreply.github.com>
2023-12-05 20:08:54 +0000

    This is a mess... slow as hell.. tried many things. This can make it ... very slowly... need to think of a better way.

--- 2023/05/p05.py
+++ 2023/05/p05.py
@@ -1,10 +1,19 @@
 import os
+import time
+import functools
 
 # input = "demo"
 input = "input"
 
+steps = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"]
+rules = {}
 
-class FroTo:
+
+def compara(a, b):
+    return b[1] - a[1]
+
+
+class FromTo:
     def __init__(self, txt: str) -> None:
         txt = txt.split()
         ft = txt[0].split("-")
@@ -13,39 +22,66 @@ class FroTo:
         i = iter(txt[2::])
         self.t = []
         for a, b, c in zip(i, i, i):
-            self.t.append((int(a), int(b), int(c)))
+            self.t.append([int(a), int(b), int(c)])
+        self.t.sort(key=functools.cmp_to_key(compara))
+        print(self.t)
 
-    def calc(self, v: int):
+    def calc(self, v: int) -> int:
         o = v
         for r in self.t:
-            if v - r[1] >= 0 and v - r[1] - r[2] < 0:
-                o = r[0] + v - r[1]
+            sh = r[0] - r[1]
+            if v < r[1] + r[2] and v >= r[1]:
+                o = v + sh
+                return o
         return o
 
 
 def parte_a(data):
-    total = 0
     seeds = [int(x) for x in data[0].split()[1::]]
-    rules = {}
-    for el in data[1::]:
-        ss = FroTo(el)
-        rules[ss.origin] = ss
-    # print(rules)
-    steps = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"]
     locations = []
     for seed in seeds:
         val = seed
         for step in steps:
             val = rules[step].calc(val)
         locations.append(val)
-        print(seed, val) 
-    print(min(locations), locations) 
-    print("-- Total parte A:\t", total)
+    print("-- Total parte A:\t", min(locations))
 
 
 def parte_b(data):
-    total = 0
-    print("-- Total parte B:\t", total)
+    min_locations = 999_999_999_999
+
+    seeds = iter([int(x) for x in data[0].split()[1::]])
+
+    tstart = time.time()
+    telapsed = 0
+
+    for seed, number in zip(seeds, seeds):
+        # print(seed, number)
+        max = seed + number
+        i = seed
+    
+        for i in range(seed, seed + number):
+            val = passo(i)
+
+            if val < min_locations:
+                min_locations = val
+
+            # continue
+
+            if (max - i) % 1_000_000 == 0:
+                now = time.time() - tstart
+                print("-- ", seed, max, i, max - i, min_locations, now, now - telapsed)
+                telapsed = now
+                # return
+            
+
+    print("-- Total parte B:\t", min_locations)
+
+
+def passo(val):
+    for step in steps:
+        val = rules[step].calc(val)
+    return val
 
 
 data: list[str] = (
@@ -54,8 +90,12 @@ data: list[str] = (
     .strip()
     .split("\n\n")
 )
+for el in data[1::]:
+    ss = FromTo(el)
+    rules[ss.origin] = ss
+
 
 print("---------------")
 
-parte_a(data)
+# parte_a(data)
 parte_b(data)

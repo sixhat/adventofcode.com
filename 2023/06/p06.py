625208bce957ef26006d8ebf077ae948ce877ef6
Dave <sixhat@users.noreply.github.com>
2023-12-08 19:31:56 +0000

    New part B just in 4 lines of code

--- 2023/06/p06.py
+++ 2023/06/p06.py
@@ -1,7 +1,19 @@
 import os
+import time
 
 # input = "demo"
 input = "input"
+
+start = 0
+
+
+def tic():
+    global start
+    start = time.time()
+
+
+def toc():
+    print(time.time() - start)
 
 
 def parte_a(data):
@@ -10,40 +22,26 @@ def parte_a(data):
     d = [int(x) for x in data[1].split()[1:]]
     dp = list(zip(t, d))
 
-    ways_to_beat = []
+    ways_to_beat: list[int] = []
     for t, d in dp:
         n = 0
         for i in range(t + 1):
             if (t - i) * i > d:
                 n += 1
-        if n>0:
+        if n > 0:
             ways_to_beat.append(n)
-    
+
     for a in ways_to_beat:
         total = total * a
 
     print("-- Total parte A:\t", total)
 
 
-def parte_b(data):
-    total = 1
+def parte_b(data: list[str]):
+    t: int = int(data[0].split(":")[1].replace(" ", "").strip())
+    d: int = int(data[1].split(":")[1].replace(" ", "").strip())
     
-    t = [int(data[0].split(":")[1].replace(" ", "").strip())]
-    d = [int(data[1].split(":")[1].replace(" ", "").strip())]
-
-    dp = list(zip(t, d))
-
-    ways_to_beat = []
-    for t, d in dp:
-        n = 0
-        for i in range(t + 1):
-            if (t - i) * i > d:
-                n += 1
-        if n>0:
-            ways_to_beat.append(n)
-    
-    for a in ways_to_beat:
-        total = total * a
+    total = [(t - i) * i > d for i in range(t + 1)].count(True)
     print("-- Total parte B:\t", total)
 
 
@@ -54,5 +52,7 @@ data: list[str] = (
     .split("\n")
 )
 
-parte_a(data)
+# parte_a(data)
+tic()
 parte_b(data)
+toc()

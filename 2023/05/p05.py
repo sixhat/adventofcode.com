c23586a51b40d7f9c89de1b6260dc7fac968545f
Dave <sixhat@users.noreply.github.com>
2023-12-12 00:30:30 +0000

    Finally fixed part B of p05.

--- 2023/05/p05.py
+++ 2023/05/p05.py
@@ -1,3 +1,7 @@
+# %% [markdown]
+# Day 5 Parte B - Tentar perceber o algoritmo de trabalhar com Ranges.
+
+# %%
 import os
 import time
 import functools
@@ -8,93 +12,112 @@ input = "input"
 steps = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"]
 rules = {}
 
-
-def compara(a, b):
-    return b[1] - a[1]
+data: list[str] = open(f"{input}", "r").read().strip().split("\n\n")
 
 
+# %%
 class FromTo:
     def __init__(self, txt: str) -> None:
+        # print("== FromTo =====")
         txt = txt.split()
         ft = txt[0].split("-")
         self.origin = ft[0]
         self.destination = ft[2]
         i = iter(txt[2::])
-        self.t = []
+        self.regras = []
         for a, b, c in zip(i, i, i):
-            self.t.append([int(a), int(b), int(c)])
-        self.t.sort(key=functools.cmp_to_key(compara))
-        print(self.t)
+            self.regras.append([int(a), int(b), int(c)])
+        # self.regras.sort(key=functools.cmp_to_key(compara))
+        # print(self.regras)
 
     def calc(self, v: int) -> int:
-        o = v
-        for r in self.t:
+        for r in self.regras:
             sh = r[0] - r[1]
-            if v < r[1] + r[2] and v >= r[1]:
-                o = v + sh
-                return o
-        return o
+            if r[1] <= v < r[1] + r[2]:
+                return v + sh
+        return v
 
-def parte_a(data):
-    seeds = [int(x) for x in data[0].split()[1::]]
+    def calc_ranges(self, intervalos: list[tuple[int, int]]) -> list[tuple[int, int]]:
+        # print(" calc_ranges\t", intervalos)
+        out_intervalos = []
+
+        for regra in self.regras:
+            delta = regra[0] - regra[1]
+            ir = (regra[1], regra[1] + regra[2])
+
+            novos_intervalos = []
+            while intervalos:
+                intervalo = intervalos.pop()
+
+                left = (intervalo[0], min(intervalo[1], ir[0]))
+
+                inner = (
+                    max(intervalo[0], ir[0]) + delta,
+                    min(intervalo[1], ir[1]) + delta,
+                )
+
+                right = (max(intervalo[0], ir[1]), intervalo[1])
+
+                if left[1] > left[0]:
+                    novos_intervalos.append(left)
+
+                if inner[1] > inner[0]:
+                    out_intervalos.append(inner)
+
+                if right[1] > right[0]:
+                    novos_intervalos.append(right)
+
+            intervalos = novos_intervalos
+        return out_intervalos + intervalos
+
+
+# %%
+for el in data[1::]:
+    ss = FromTo(el)
+    rules[ss.origin] = ss
+
+
+# %%
+def calc_seeds_1by1(seeds):
     locations = []
     for seed in seeds:
         val = seed
         for step in steps:
             val = rules[step].calc(val)
         locations.append(val)
-    print("-- Total parte A:\t", min(locations))
+    return locations
 
 
-def parte_b(data):
-    min_locations = 999_999_999_999
-
-    seeds = iter([int(x) for x in data[0].split()[1::]])
-
-    tstart = time.time()
-    telapsed = 0
-
-    for seed, number in zip(seeds, seeds):
-        # print(seed, number)
-        max = seed + number
-        i = seed
-    
-        for i in range(seed, seed + number):
-            val = passo(i)
-
-            if val < min_locations:
-                min_locations = val
-
-            # continue
-
-            if (max - i) % 1_000_000 == 0:
-                now = time.time() - tstart
-                print("-- ", seed, max, i, max - i, min_locations, now, now - telapsed)
-                telapsed = now
-                # return
-            
-
-    print("-- Total parte B:\t", min_locations)
+# %%
+def parte_a(data):
+    seeds = [int(x) for x in data[0].split()[1::]]
+    print("-- Total parte A:\t", min(calc_seeds_1by1(seeds)))
 
 
-def passo(val):
-    for step in steps:
-        val = rules[step].calc(val)
-    return val
+# %%
+parte_a(data)
 
 
-data: list[str] = (
-    open(os.path.dirname(os.path.realpath(__file__)) + f"/{input}", "r")
-    .read()
-    .strip()
-    .split("\n\n")
-)
-for el in data[1::]:
-    ss = FromTo(el)
-    rules[ss.origin] = ss
-
-
-print("---------------")
-
+# %%
 # parte_a(data)
-parte_b(data)
+# parte_b(data)
+min_locations = 999_999_999_999
+
+# need ranges here [a,b) (b is exclusive)
+a_seeds = [int(x) for x in data[0].split()[1::]]
+b_seeds = list(zip(a_seeds[::2], a_seeds[1::2]))
+ranges = [(a[0], a[0] + a[1]) for a in b_seeds]
+
+# print("---\t", ranges)
+# ranges = [(90, 110)]
+P2 = []
+ran = ranges
+for ran in ranges:
+    intervalos = [ran]
+    # print(r)
+    for step in steps:
+        # print("-- step\t", step)
+        intervalos = rules[step].calc_ranges(intervalos)
+        # print("OUT ranges\t", R)
+    P2.append(min(intervalos)[0])
+print("-- Total parte B:\t", min(P2))
---
+++ 2023/05/p05c.py
@@ -0,0 +1,100 @@
+import os
+import time
+import functools
+
+# input = "demo"
+input = "input"
+
+steps = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity"]
+rules = {}
+
+
+def compara(a, b):
+    return b[1] - a[1]
+
+
+class FromTo:
+    def __init__(self, txt: str) -> None:
+        txt = txt.split()
+        ft = txt[0].split("-")
+        self.origin = ft[0]
+        self.destination = ft[2]
+        i = iter(txt[2::])
+        self.t = []
+        for a, b, c in zip(i, i, i):
+            self.t.append([int(a), int(b), int(c)])
+        self.t.sort(key=functools.cmp_to_key(compara))
+        print(self.t)
+
+    def calc(self, v: int) -> int:
+        o = v
+        for r in self.t:
+            sh = r[0] - r[1]
+            if v < r[1] + r[2] and v >= r[1]:
+                o = v + sh
+                return o
+        return o
+
+def parte_a(data):
+    seeds = [int(x) for x in data[0].split()[1::]]
+    locations = []
+    for seed in seeds:
+        val = seed
+        for step in steps:
+            val = rules[step].calc(val)
+        locations.append(val)
+    print("-- Total parte A:\t", min(locations))
+
+
+def parte_b(data):
+    min_locations = 999_999_999_999
+
+    seeds = iter([int(x) for x in data[0].split()[1::]])
+
+    tstart = time.time()
+    telapsed = 0
+
+    for seed, number in zip(seeds, seeds):
+        # print(seed, number)
+        max = seed + number
+        i = seed
+    
+        for i in range(seed, seed + number):
+            val = passo(i)
+
+            if val < min_locations:
+                min_locations = val
+
+            # continue
+
+            if (max - i) % 1_000_000 == 0:
+                now = time.time() - tstart
+                print("-- ", seed, max, i, max - i, min_locations, now, now - telapsed)
+                telapsed = now
+                # return
+            
+
+    print("-- Total parte B:\t", min_locations)
+
+
+def passo(val):
+    for step in steps:
+        val = rules[step].calc(val)
+    return val
+
+
+data: list[str] = (
+    open(os.path.dirname(os.path.realpath(__file__)) + f"/{input}", "r")
+    .read()
+    .strip()
+    .split("\n\n")
+)
+for el in data[1::]:
+    ss = FromTo(el)
+    rules[ss.origin] = ss
+
+
+print("---------------")
+
+# parte_a(data)
+parte_b(data)

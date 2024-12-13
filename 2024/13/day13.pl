4097201c8976b5b668a5349b08298118be367c61
Dave <sixhat@users.noreply.github.com>
2024-12-13 22:57:58 +0000

    Update day13.pl

--- 2024/13/day13.pl
+++ 2024/13/day13.pl
@@ -5,58 +5,55 @@ package Day13;
 use strict;
 use warnings;
 use feature 'say';
-my $ax;
-my $ay;
-my $bx;
-my $by;
-my $px;
-my $py;
+use version; our $VERSION = qv(0.9.9);
+use Math::Round;
+use Readonly;
+use Carp;
+
+Readonly::Scalar my $EPSILON     => 0.001;
+Readonly::Scalar my $PART2FACTOR => 10_000_000_000_000;
+
+my ( $ax, $ay, $bx, $by, $px, $py );
 
 sub _int {
     my $v = shift;
-    return abs( $v - int( $v + 0.5 ) ) < 0.0001;
-
-    # return $v =~ /\d+/;
+    return abs( $v - round($v) ) < $EPSILON;
 }
 
 my $result = 0;
-my $p2    = 1;    # set to 1 for part 2
+my $p2     = 1;    # set to 1 for part 2
 while ( my $line = <> ) {
-    chomp($line);
+    chomp $line;
     $px = 0;
     $py = 0;
-
-    if ( $line =~ m/Button A: X\+(\d+), Y\+(\d+)/ ) {
+    if ( $line =~
+        m{Button[\N{SPACE}]+A:[\N{SPACE}]+X[+](\d+),[\N{SPACE}]+Y[+](\d+)}xms )
+    {
         $ax = $1;
         $ay = $2;
     }
-    if ( $line =~ m/Button B: X\+(\d+), Y\+(\d+)/ ) {
+    if ( $line =~
+        m{Button[\N{SPACE}]+B:[\N{SPACE}]+X[+](\d+),[\N{SPACE}]+Y[+](\d+)}xms )
+    {
         $bx = $1;
         $by = $2;
     }
-    if ( $line =~ m/Prize: X=(\d+), Y=(\d+)/ ) {
-
+    if ( $line =~ m{Prize:[\N{SPACE}]+X=(\d+),[\N{SPACE}]+Y=(\d+)}xms ) {
         if ($p2) {
-            $px = $1 + 10000000000000;
-            $py = $2 + 10000000000000;
+            $px = $1 + $PART2FACTOR;
+            $py = $2 + $PART2FACTOR;
         }
         else {
             $px = $1;
             $py = $2;
         }
-
-        print "\n$ax n + $bx m = $px\n";
-        print "$ay n + $by m = $py\n";
         my $m = ( ( $py - $px * $ay / $ax ) / ( $by - $bx * $ay / $ax ) );
         my $n = ( ( $px - $bx * $m ) / $ax );
-        say "m: ", $m;
-        say "n: ", $n;
-
         if ( _int($m) && _int($n) ) {
-            $result += 3 * $n + $m;
-            say "----- Part 1: $result\n";
+            $result += $n + $n + $n + $m;
         }
     }
 }
 
-say "\n\n> Result: $result";
+say "\n\n> Result: $result" || croak 'died';
+1;
